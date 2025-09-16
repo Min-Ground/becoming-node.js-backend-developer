b58f843d6a5d51043ca49041dca2d6298e32556f
anxi01 <tjdals2893@naver.com>
2025-09-16 21:59:10 +0900

    9.6: .ts 파일을 사용해 커스텀 환경 설정 파일 사용

--- chapter9/config-test/src/app.module.ts
+++ chapter9/config-test/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { WeatherModule } from './weather/weather.module';
+import config from './configs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ 
+  imports: [ConfigModule.forRoot({
     isGlobal: true, // 전역 모듈 설정하기
     envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`, // env 경로 지정
+    load: [config] // 커스텀 파일 설정
   }), WeatherModule],
   controllers: [AppController],
   providers: [AppService],
