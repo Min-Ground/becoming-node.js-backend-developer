7b58d2a3e471ecc177e8d5d6ce298bd60f70f5b2
anxi01 <tjdals2893@naver.com>
2025-09-16 18:26:35 +0900

    9.3: @nestjs/config 설치 및 ConfigModule 테스트

---
+++ chapter9/config-test/src/app.module.ts
@@ -0,0 +1,11 @@
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
