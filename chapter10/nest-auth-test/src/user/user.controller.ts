a4f50d0982fca990242fe58c1f69199b6b5298f8
anxi01 <tjdals2893@naver.com>
2025-09-16 22:45:39 +0900

    10.2: 유저 모듈의 엔티티, 서비스, 컨트롤러 생성

--- chapter10/nest-auth-test/src/user/user.controller.ts
+++ chapter10/nest-auth-test/src/user/user.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) { }
+
+    @Post('/create')
+    createUser(@Body() user: User) {
+        return this.userService.createUser(user);
+    }
+
+    @Get('/getUser/:email')
+    async getUser(@Param('emali') email: string) {
+        const user = await this.userService.getUser(email);
+        return user;
+    }
+
+    @Put('/update/:email')
+    updateUser(@Param('email') email: string, @Body() user: User) {
+    console.log(user);
+        return this.userService.updateUser(email, user);
+    }
+
+    @Delete('/delete/:email')
+    deleteUser(@Param('email') email: string) {
+        return this.userService.deleteUser(email);
+    }
+}
